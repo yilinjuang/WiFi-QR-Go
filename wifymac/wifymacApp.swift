17eddb4ed8cc763a59930ca1f7445951b3768785
Yi-Lin Juang <frankyjuang@gmail.com>
2025-03-17 22:55:41 +0900

    Quit app when close window

--- wifymac/wifymacApp.swift
+++ wifymac/wifymacApp.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 import AppKit
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+}
+
 @main
 struct WifyApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
